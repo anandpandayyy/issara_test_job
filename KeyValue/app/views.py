b4a1c9779cd3965aee50651c844e1145a8bcdbc3
anandpandayyy <anandpandayyy@gmail.com>
2022-09-13 16:32:21 +0530

    implement ttl

--- KeyValue/app/views.py
+++ KeyValue/app/views.py
@@ -2,12 +2,16 @@ from rest_framework.views import APIView
 from .models import Store
 from rest_framework.response import Response
 from django.db import transaction
+from django.utils import timezone
+from datetime import timedelta
 
 class StoreView(APIView):
+   
     def get_queryset(self,key):
         qs = Store.objects.all()
         if key:
             qs = qs.filter(key__in=key.split(','))
+            qs.update(created_at=timezone.now()+timedelta(minutes=5))
         return {i['key']:i['value'] for i in qs.values('key','value')}
 
     def get(self, request):
@@ -32,6 +36,7 @@ class StoreView(APIView):
                     obj=Store.objects.get(key=k)
                     if obj:
                         obj.value=v
+                        obj.created_at=timezone.now()+timedelta(minutes=5)
                         obj.save()                       
                 except:
                     return Response({'errors':f'This key {k} does not exixts'},status=400)        
